20772dbbf797cda199caab7c350f3e0ed0b1c81e
MrZhang365 <3288492688@qq.com>
2022-10-06 01:34:38 +0800

    汉化session命令

---
+++ 一些汉化的命令/core/session.js
@@ -0,0 +1,13 @@
+/*
+  Description: Create a new socket session or restore previous session
+*/
+
+// module main
+export async function run() { }
+
+export const info = {
+  name: 'session',
+  description: '恢复上一次的会话（现在不可用）',
+  usage: `
+    API: { cmd: 'session', id: '<previous session>' }`
+};
