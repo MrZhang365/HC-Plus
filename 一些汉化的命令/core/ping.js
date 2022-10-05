e39f37bc0c9c854e5d138712bf40a9499cf3724b
MrZhang365 <3288492688@qq.com>
2022-10-06 01:33:29 +0800

    汉化ping命令

---
+++ 一些汉化的命令/core/ping.js
@@ -0,0 +1,11 @@
+/*
+  Description: This module is only in place to supress error notices legacy sources may get
+*/
+
+// module main
+export async function run() { }
+
+export const info = {
+  name: 'ping',
+  description: '此模块用于防止因长时间没有活动而导致连接断开的悲剧发生。',
+};
